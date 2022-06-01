aeffab9aa80765f2904adc3aead3c0420c6ca9e4
ErikPC <epardillo@cifpfbmoll.eu>
2022-06-01 17:20:25 +0200

    feat: añadir busqueda usuaria

--- one/src/main/java/edu/poniperro/resources/service/SerivceOlli.java
+++ one/src/main/java/edu/poniperro/resources/service/SerivceOlli.java
@@ -1,8 +1,16 @@
 package edu.poniperro.resources.service;
 
+import java.util.Optional;
+
 import javax.enterprise.context.ApplicationScoped;
+
+import edu.poniperro.resources.entities.Usuaria;
 
 @ApplicationScoped
 public class SerivceOlli {
 
+    public Usuaria cargaUsuaria(String nombre) {
+        Optional<Usuaria> usuaria = Usuaria.findByIdOptional(nombre);
+        return usuaria.isPresent() ? usuaria.get() : new Usuaria();
+    }
 }
