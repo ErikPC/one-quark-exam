fa378e0fcff08e5b9e0dbe07609182da89655b24
ErikPC <epardillo@cifpfbmoll.eu>
2022-06-01 17:17:38 +0200

    feat: hacer que los campos no inicien en null

--- one/src/main/java/edu/poniperro/resources/entities/Usuaria.java
+++ one/src/main/java/edu/poniperro/resources/entities/Usuaria.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,12 +17,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 @Table(name = "t_users")
-public class Usuaria {
+public class Usuaria extends PanacheEntityBase {
 
     @Id
     @Column(name = "user_nom")
-    private String nombre;
+    private String nombre = "";
 
     @Column(name = "user_prop")
-    private Integer destreza;
+    private Integer destreza = 0;
 }
