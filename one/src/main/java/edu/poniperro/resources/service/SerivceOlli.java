49144c7a3a164b55ae2397b3ccdc18f670b31c38
ErikPC <epardillo@cifpfbmoll.eu>
2022-06-01 18:01:47 +0200

    feat: añadir que devuelva una lista de Ordenes

--- one/src/main/java/edu/poniperro/resources/service/SerivceOlli.java
+++ one/src/main/java/edu/poniperro/resources/service/SerivceOlli.java
@@ -1,9 +1,13 @@
 package edu.poniperro.resources.service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import edu.poniperro.resources.entities.Item;
+import edu.poniperro.resources.entities.Orden;
 import edu.poniperro.resources.entities.Usuaria;
 
 @ApplicationScoped
@@ -12,5 +16,23 @@ public class SerivceOlli {
     public Usuaria cargaUsuaria(String nombre) {
         Optional<Usuaria> usuaria = Usuaria.findByIdOptional(nombre);
         return usuaria.isPresent() ? usuaria.get() : new Usuaria();
+    }
+
+    public Item cargaItem(String nombre) {
+        Optional<Item> item = Item.findByIdOptional(nombre);
+        return item.isPresent() ? item.get() : new Item();
+
+    }
+
+    public List<Orden> cargaOrden(String nombre) {
+        List<Orden> filtrada = todasOrdenes().stream().filter(o -> o.getUser().getNombre().equals(nombre))
+                .collect(Collectors.toList());
+        return filtrada;
+    }
+
+    private List<Orden> todasOrdenes() {
+        List<Orden> listOrden = Orden.findAll().list();
+        return listOrden;
+
     }
 }
